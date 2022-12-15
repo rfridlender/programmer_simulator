cc273e94600715c326ba502827efecb9180dfcfa
Robert Fridlender <robertfridlender@gmail.com>
2022-12-15 16:43:44 -0500

    add functionality; bug with movement

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
+// Things to do/not forget:
+
+// Computer bug screen can be red half transparent radial gradient flashing
+
+// Fix sprite speed functionality, gonna have to make sure that everything is ratio'd to the speed, otherwise, if speed is half as small, the map will also be half as small if not adjusted to it
+
+// Create dynamic desk placement set up, make sure to get rid of all the hard code related stuff for testing initial functionality. All of the work should be within js, choosing a random location, making the desks range and tiles it occupies, and then sending it to the DOM
+
+
+
+
+// look up table where key is the cood and bondry is the value 
+
+// /`2-2`: {right: flase}
 /*------------------------ Cached Element References ------------------------*/
 
-const programmer = document.querySelector(`.sprite`);
-const programmerImg = document.querySelector(`.sprite-image`);
+const programmerAura = document.querySelector(`.sprite`);
+const programmer = document.querySelector(`.sprite-image`);
 const screen = document.querySelector(`.screen`);
+
+
+// const desk = document.querySelector(`.desk-one`);
+
 
 /*-------------------------------- Constants --------------------------------*/
 
@@ -13,17 +31,19 @@ const keys = [
   {code: 38, name: `up`}
 ];
 const tileSize = programmer.clientWidth;
-const outerWallThickness = {x: 1, y: 2};
+const outerWallThickness = {right: 1, left: 1, bottom: 1, top: 2};
 const mapSize = {
-  x: pixelTranslator(+getComputedStyle(screen).width.replace(`px`, ``)),
-  y: pixelTranslator(+getComputedStyle(screen).height.replace(`px`, ``))
+  x: screen.clientWidth / 48,
+  y: screen.clientHeight / 48,
 };
-const runAnimationDuration = +getComputedStyle(programmer).transitionDuration.replace(`s`, ``) * 1000;
+const tiles = [];
+const obstacles = [];
 
+console.log(mapSize);
+console.log(tileSize);
 /*---------------------------- Variables (state) ----------------------------*/  
 
 let step = 1;
-let tiles = [];
 
 /*---------------------------- Classses / Object -------------------------------------*/
 
@@ -37,72 +57,116 @@ class Tile {
 
 for (let x = 0; x < mapSize.x; x++) {
   for (let y = 0; y < mapSize.y; y++) {
-    if (x === outerWallThickness.x && y === outerWallThickness.y) {
-      tiles.push(new Tile(x, y, false, true, false, true));
-    } else if (x === mapSize.x - outerWallThickness.x - 1 && y === outerWallThickness.y) {
-      tiles.push(new Tile(x, y, true, false, false, true));
-    } else if (x === outerWallThickness.x && y === mapSize.y - 2) {
-      tiles.push(new Tile(x, y, false, true, true, false));
-    } else if (x === mapSize.x - outerWallThickness.x - 1 && y === mapSize.y - 2) {
-      tiles.push(new Tile(x, y, true, false, true, false));
-    } else if (x === outerWallThickness.x) {
+    if (x === outerWallThickness.left && y !== outerWallThickness.top) {
       tiles.push(new Tile(x, y, false, true, false, false));
-    } else if (x === mapSize.x - outerWallThickness.x - 1) {
+    } else if (x === mapSize.x - outerWallThickness.right - 1 && y !== outerWallThickness.top) {
       tiles.push(new Tile(x, y, true, false, false, false));
-    } else if (y === outerWallThickness.y) {
+    } else if (y === outerWallThickness.top && x !== outerWallThickness.left && x !== outerWallThickness.right) {
       tiles.push(new Tile(x, y, false, false, false, true));
-    } else if (y === mapSize.y - 2) {
+    } else if (y === mapSize.y - outerWallThickness.bottom - 1 && x !== outerWallThickness.left && x !== mapSize.x - outerWallThickness.right - 1) {
       tiles.push(new Tile(x, y, false, false, true, false));
+    } else if (x === outerWallThickness.left && y === outerWallThickness.top) {
+      tiles.push(new Tile(x, y, false, true, false, true));
+    } else if (x === mapSize.x - outerWallThickness.right - 1 && y === outerWallThickness.top) {
+      tiles.push(new Tile(x, y, true, false, false, true));
+    } else if (x === outerWallThickness.left && y === mapSize.y - outerWallThickness.bottom - 1) {
+      tiles.push(new Tile(x, y, false, true, true, false));
+    } else if (x === mapSize.x - outerWallThickness.right - 1 && y === mapSize.y - outerWallThickness.bottom - 1) {
+      tiles.push(new Tile(x, y, true, false, true, false));
     } else {
       tiles.push(new Tile(x, y, false, false, false, false));
     };
   };
 };
 
+
+// object = {`2-2`: {} }
+
+
+
+// class Desks {
+//   constructor(x, y, right, left, bottom, top) {
+//     this.pos = {xMin: x, xMax: , yMin: y, yMax: }
+//   }
+// };
+
+// const deskOne = obstacleRangeFinder(desk);
+
+
+
+
+
 const sprite = {
-  pos: {x: 2, y: 2},
+  spriteDim: {x: programmer.clientWidth, y: programmer.clientHeight},
+  currentTile: {pos: {x: 2, y: 2}, boundary: {right: false, left: false, bottom: false, top: true}, terminal: false},
+  speedTilesPerPress: 1,
+  runAnimationDuration: +getComputedStyle(programmerAura).transitionDuration.replace(`s`, ``) * 1000,
+  spriteLayer: -1,
+  updateTile() {
+    console.log(`new pos of sprite`, this.currentTile);
+    const foundTile = tiles.find(tile => {
+      return tile.pos.x === this.currentTile.pos.x && tile.pos.y === this.currentTile.pos.y;
+      // return tile.pos === this.currentTile.pos;
+    });
+    console.log(`found tile`, foundTile);
+    const newObj = {
+      ...this.currentTile, 
+      ...foundTile
+    }
+    console.log(`newobj`,newObj);
+    this.currentTile = newObj;
+    console.log(`new tile`, this.currentTile);
+  },
+  // updateLayer() {
+
+  // },
   turn(key) {
-      programmerImg.src = `./assets/sprite_idle_${key.name}.gif`;
+      programmer.src = `./assets/sprite_idle_${key.name}.gif`;
   },
   moveRight() {
-    programmer.style.left = `${pixelTranslator(this.pos.x) + tileSize}px`;
-    programmerImg.src = `./assets/sprite_run_right_${step}.gif`
+    programmerAura.style.left = `${pixelTranslator(this.currentTile.pos.x) + (this.spriteDim.x * this.speedTilesPerPress)}px`;
+    programmer.src = `./assets/sprite_run_right_${step}.gif`
     setTimeout(() => {
-      programmerImg.src = `./assets/sprite_idle_right.gif`;
-    }, runAnimationDuration);
+      programmer.src = `./assets/sprite_idle_right.gif`;
+    }, this.runAnimationDuration);
     step *= -1;
-    this.pos.x++;
+    console.log(`working`);
+    this.currentTile.pos.x += this.speedTilesPerPress;
+    this.updateTile();
   },
   moveLeft() {
-    programmer.style.left = `${pixelTranslator(this.pos.x) - tileSize}px`;
-    programmerImg.src = `./assets/sprite_run_left_${step}.gif`
+    programmerAura.style.left = `${pixelTranslator(this.currentTile.pos.x) - (this.spriteDim.x * this.speedTilesPerPress)}px`;
+    programmer.src = `./assets/sprite_run_left_${step}.gif`
     setTimeout(() => {
-      programmerImg.src = `./assets/sprite_idle_left.gif`;
-    }, runAnimationDuration);
+      programmer.src = `./assets/sprite_idle_left.gif`;
+    }, this.runAnimationDuration);
     step *= -1;
-    this.pos.x--;
+    this.currentTile.pos.x -= this.speedTilesPerPress;
+    this.updateTile();
   },
   moveDown() {
-    programmer.style.top = `${pixelTranslator(this.pos.y)}px`;
-    programmerImg.src = `./assets/sprite_run_down_${step}.gif`
+    programmerAura.style.top = `${(pixelTranslator(this.currentTile.pos.y)) * this.speedTilesPerPress}px`;
+    programmer.src = `./assets/sprite_run_down_${step}.gif`
     setTimeout(() => {
-      programmerImg.src = `./assets/sprite_idle_down.gif`;
-    }, runAnimationDuration);
+      programmer.src = `./assets/sprite_idle_down.gif`;
+    }, this.runAnimationDuration);
     step *= -1;
-    this.pos.y++;
+    this.currentTile.pos.y += this.speedTilesPerPress;
+    // this.updateTile();
   },
   moveUp() {
-    programmer.style.top = `${pixelTranslator(this.pos.y) - tileSize * 2}px`;
-    programmerImg.src = `./assets/sprite_run_up_${step}.gif`
+    programmerAura.style.top = `${pixelTranslator(this.currentTile.pos.y) - (this.spriteDim.y  * this.speedTilesPerPress)}px`;
+    programmer.src = `./assets/sprite_run_up_${step}.gif`
     setTimeout(() => {
-      programmerImg.src = `./assets/sprite_idle_up.gif`;
-    }, runAnimationDuration);
+      programmer.src = `./assets/sprite_idle_up.gif`;
+    }, this.runAnimationDuration);
     step *= -1;
-    this.pos.y--;
+    this.currentTile.pos.y -= this.speedTilesPerPress;
+    // this.updateTile();
   },
 };
-console.log(sprite.pos);
-/*----------------------------- Event Listeners -----------------------------*/
+
+/*----------------------------- Event Listeners ----------------------------*/
 
 document.addEventListener(`keydown`, handleKey);
 
@@ -110,19 +174,23 @@ document.addEventListener(`keydown`, handleKey);
 
 function handleKey(evt) {
   const key = keys.find(key => key.code === evt.keyCode);
-  const spritePos = tiles.find(tile => tile.pos.x === sprite.pos.x && tile.pos.y === sprite.pos.y);
-  console.log(`oldtile`, spritePos);
+  console.log(key);
+  console.log(`old tile left bound status`, sprite.currentTile.boundary.left);
+  if (!key) return;
   sprite.turn(key);
-  if (key.name === `right` && !spritePos.boundary.right) {
+  if (key.name === `right` && !sprite.currentTile.boundary.right) {
+    console.log(`moving right`);
     sprite.moveRight();
-  } else if (key.name === `left` && !spritePos.boundary.left) {
+  } else if (key.name === `left` && !sprite.currentTile.boundary.left) {
+    console.log(`moving left`);
     sprite.moveLeft();
-  } else if (key.name === `down` && !spritePos.boundary.bottom) {
+  } else if (key.name === `down` && !sprite.currentTile.boundary.bottom) {
+    console.log(`moving down`);
     sprite.moveDown();
-  } else if (key.name === `up` && !spritePos.boundary.top) {
+  } else if (key.name === `up` && !sprite.currentTile.boundary.top) {
+    console.log(`moving up`);
     sprite.moveUp();
   };
-  console.log(`newtile`, sprite.pos);
 };
 
 
@@ -130,4 +198,35 @@ function pixelTranslator(num) {
   return !(num % tileSize) ? num / tileSize : num * tileSize;
 };
 
-console.log(tiles.find(tile => tile.pos.x === 2 && tile.pos.y === 3));
+// function obstacleRangeFinder(obstacleNode) {
+//   const pos = {xMin: null, xMax: null, yMin: null, yMax: null};
+//   pos.xMin = pixelTranslator(obstacleNode.offsetLeft);
+//   pos.xMax = pos.xMin + pixelTranslator(obstacleNode.clientWidth) - 1;
+//   pos.yMin = pixelTranslator(obstacleNode.offsetTop);
+//   pos.yMax = pos.yMin + pixelTranslator(obstacleNode.clientHeight) - 1;
+//   return pos;
+// };
+
+// function assignObjectBoundaries(obstacleRange) {
+//   console.log(obstacleRange);
+//   // tiles.forEach(tile => {
+//   //   for (let x = obstacleRange.xMin; x <= obstacleRange.xMax; x++) {
+//   //     for (let y = obstacleRange.yMin; x <= obstacleRange.yMax; y++) {
+//   //       if (x === obstacleRange.xMin && y !== obstacleRange.yMin && y!== obstacleRange.yMax) {
+//   //         tile.boundary.right = true;
+//   //       };
+//   //     };
+//   //   };
+//   // });
+// };
+
+// assignObjectBoundaries(obstacleRangeFinder(desk));
+
+// console.log(tiles);
+
+
+
+const sprite = {
+  posX: 2,
+  posY: 2
+}
